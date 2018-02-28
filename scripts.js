2e3e791ccc812ba8af1f36ca31e2c5a45d71d70b
karapeneguy <karapeneguy@Karas-MacBook-Pro.local>
2018-02-27 18:28:15 -0600

    Added image folder
    Added images

--- scripts.js
+++ scripts.js
@@ -1,23 +1,7 @@
-var visibleText= "Show Less";
-var hiddenText= "Show More";
 
 
 
-$(document).ready(function(){
-    
-   $(".info-box button").click(function(){
-       $(this).toggleClass("visible");
-       
-       if($(this).hasClass("visible") ){
-           $(this).html("Show Less");
-       }else{
-           $(this).html("Show More");
-       }
-       
-       
-      $(this).next().toggleClass(visibleText);
-   });
-});
+
 
 
 
