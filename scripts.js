d6b481393db38813395fbf822168226aa17b621c
karapeneguy <karapeneguy@Karas-MacBook-Pro.local>
2018-03-06 12:14:43 -0600

    Lots of mistakes

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
+var visibleText = "Show Less"
+var hiddenText = "Full Biography"
 
-
-
-
-
-
+$(document).ready(function () {
+    $(".morebio").click(function () {
+        $(this).toggleClass("visible");
+        if ($(this).hasClass("visible")) {
+            $(this).html(hiddenText);
+        }
+        else {
+            $(this).html(visibleText);
+        }
+        $(this).next().toggleClass("visible");
+    });
+})
 
 
